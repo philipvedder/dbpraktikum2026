303883598b74bb15948192302c5b660582118686
Anna Minakova <annuta.minakova@gmail.com>
2026-05-27 18:58:10 +0200

    added review repository

---
+++ loader/src/main/java/de/unileipzig/dbpraktikum/loader/db/repository/ReviewRepository.java
@@ -0,0 +1,47 @@
+package de.unileipzig.dbpraktikum.loader.db.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import de.unileipzig.dbpraktikum.loader.model.Review;
+
+/**
+ * Repository to interact with the DB Table:
+ * Rezension
+ */
+public class ReviewRepository {
+    private final CustomerRepository customerRepository;
+
+    /**
+     * Initialize required Repositories.
+     */
+    public ReviewRepository() {
+        this.customerRepository = new CustomerRepository();
+    }
+
+    public void insert(Connection con, Review review) throws SQLException {
+        String sql = """
+            INSERT INTO media_store.rezension (
+                kunde_id,
+                produkt_nr,
+                rezensionszeitpunkt,
+                punkte,
+                rezensionstext
+            )
+            VALUES (?, ?, ?, ?, ?)
+        """;
+
+        try (PreparedStatement stmt = con.prepareStatement(sql)) {
+            Long customerId = customerRepository.findOrCreate(con, review.userName());
+
+            stmt.setLong(1, customerId);
+            stmt.setString(2, review.productId());
+            stmt.setTimestamp(3, review.reviewTimestamp());
+            stmt.setInt(4, review.rating());
+            stmt.setString(5, review.reviewText());
+
+            stmt.executeUpdate();
+        }
+    }
+}
