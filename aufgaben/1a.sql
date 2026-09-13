87db0959adfc6eadf5f843a6a2cde273706a7763
Anna Minakova <annuta.minakova@gmail.com>
2026-09-13 19:40:33 +0200

    Fix offer mapping constraints

--- aufgaben/1a.sql
+++ aufgaben/1a.sql
@@ -172,11 +172,12 @@ CREATE TABLE filiale (
 
 CREATE TABLE angebot (
   angebot_id BIGINT GENERATED ALWAYS AS IDENTITY,
-  filiale_id BIGINT,
-  produkt_nr VARCHAR(10),
+  filiale_id BIGINT NOT NULL,
+  produkt_nr VARCHAR(10) NOT NULL,
   preis NUMERIC NOT NULL,
   waehrung VARCHAR(8) NOT NULL,
   zustand TEXT NOT NULL,
+  CONSTRAINT pk_angebot PRIMARY KEY (angebot_id),
   CONSTRAINT fk_angebot_filiale FOREIGN KEY (filiale_id) REFERENCES filiale (filiale_id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT fk_angebot_produkt FOREIGN KEY (produkt_nr) REFERENCES produkt (produkt_nr) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT chk_angebot_preis_pos CHECK (
